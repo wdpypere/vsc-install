c396c36e1ed529cc9dc67e4ac1d39da1e38d150a
stdweird <stijn.deweirdt@ugent.be>
2015-10-15 18:30:05 +0200

    Test that the provided namespaces and modules are importable (except for the fancylogger hack)

--- test/simple.py
+++ test/simple.py
@@ -1,9 +1,15 @@
 from unittest import TestCase, TestLoader
+import sys
+
+import vsc
+import vsc.install
 import vsc.install.shared_setup
 
 class ImportTest(TestCase):
-    """Dummy class to prove the shared_setup import works"""
+    """Dummy class to prove importing works"""
 
     def test_importok(self):
         """It's ok if we get here"""
-        self.assertTrue(True, msg='import vsc.install.shared_setup was success')
+        self.assertTrue('vsc' in sys.modules, msg='import vsc was success')
+        self.assertTrue('vsc.install' in sys.modules, msg='import vsc.install was success')
+        self.assertTrue('vsc.install.shared_setup' in sys.modules, msg='import vsc.install.shared_setup was success')
