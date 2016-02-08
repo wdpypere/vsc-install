dee464694be08caa1ce9a2a594e9dcfd786216eb
stdweird <stijn.deweirdt@ugent.be>
2016-02-08 18:20:20 +0100

    Deprecate VSCImportTest and move it to a dedicated module (the prospector imports were introducing sideeffects)

--- test/00-import.py
+++ test/00-import.py
@@ -1,5 +1,5 @@
 # don't import VSCImportTest, it will trigger the tests
-import vsc.install.testing
+import vsc.install.commontest
 
-class ImportTest(vsc.install.testing.VSCImportTest):
+class ImportTest(vsc.install.commontest.CommonTest):
     EXCLUDE_MODS = ['^vsc\.fancylogger$'] # it requires vsc-base
