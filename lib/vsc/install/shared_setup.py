cf5f25eec88198333cfd95b36f9e549566d80559
Kenneth Hoste <kenneth.hoste@ugent.be>
2017-02-23 15:07:13 +0100

    bump version

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -148,7 +148,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.10.24'
+VERSION = '0.10.25'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 
