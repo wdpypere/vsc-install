285642d416b16ddba1476b406872af1d2f9e9228
Vanessa Sochat <vsochat@stanford.edu>
2018-05-09 17:01:13 -0400

    bumping version, fixing regular expression for args

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -160,7 +160,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.10.32'
+VERSION = '0.10.33'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 
