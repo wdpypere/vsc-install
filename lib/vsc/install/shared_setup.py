de0dbdc05db9d8b58ea17dca47a58d2e1f801cca
Wouter Depypere <wouter.depypere@ugent.be>
2018-10-18 16:44:24 +0200

    remove dependency version. Should be fixed.

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -1382,7 +1382,7 @@ class vsc_setup(object):
         else:
             log.info('adding prospector to tests_require')
             tests_requires = new_target.setdefault('tests_require', [])
-            tests_requires.extend(['prospector >= 0.12.1', 'pylint<2.0.0', 'pyflakes<1.7.0,>=1.5.0'])
+            tests_requires.extend(['prospector >= 0.12.1', 'pylint'])
             new_target['tests_require'] = tests_requires
 
         if self.private_repo:
