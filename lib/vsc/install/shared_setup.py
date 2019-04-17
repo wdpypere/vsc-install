997659d1f1e5397b13409168b88e76545d306696
Andy Georges <itkovian@gmail.com>
2019-04-17 15:07:59 +0200

    Version bump to 0.12.4

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -156,7 +156,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.12.3'
+VERSION = '0.12.4'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 
