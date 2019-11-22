1409f766b8752ae1ba4616f6e0e0db2292570715
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-11-22 16:10:54 +0100

    capture and check logging output in ci tests

--- test/ci.py
+++ test/ci.py
@@ -28,11 +28,20 @@ Test CI functionality
 
 @author: Kenneth Hoste (Ghent University)
 """
+import logging
 import os
+import re
 import shutil
+import sys
 import tempfile
 
-from vsc.install.ci import gen_jenkinsfile, gen_tox_ini
+try:
+    from cStringIO import StringIO  # Python 2
+except ImportError:
+    from io import StringIO  # Python 3
+
+import vsc.install.ci
+from vsc.install.ci import gen_jenkinsfile, gen_tox_ini, write_file
 from vsc.install.testing import TestCase
 
 
@@ -44,6 +53,7 @@ class CITest(TestCase):
         super(CITest, self).setUp()
 
         self.tmpdir = tempfile.mkdtemp()
+        self.tmpdir_name = os.path.basename(self.tmpdir)
 
     def tearDown(self):
         """Test cleanup"""
@@ -51,29 +61,69 @@ class CITest(TestCase):
 
         super(CITest, self).tearDown()
 
+    def run_function(self, function, *args, **kwargs):
+        """Run specified function with specified arguments, and capture generated stdout/stderr."""
+        orig_handlers = vsc.install.ci.log.handlers[:]
+        stringio = StringIO()
+        handler = logging.StreamHandler(stringio)
+        vsc.install.ci.log.handlers = [handler]
+
+        function(*args, **kwargs)
+
+        stdout = stringio.getvalue()
+        vsc.install.ci.log.handlers = orig_handlers
+
+        return stdout
+
     def test_gen_jenkinsfile(self):
         """Test generating of Jenkinsfile."""
         os.chdir(self.tmpdir)
-        gen_jenkinsfile()
 
+        def check_stdout(stdout):
+            """Helper function to check stdout output."""
+            self.assertTrue(stdout.startswith('[Jenkinsfile]'))
+            regex = re.compile(r"^Wrote .*/%s/Jenkinsfile$" % self.tmpdir_name, re.M)
+            self.assertTrue(regex.search(stdout), "Pattern '%s' found in: %s" % (regex.pattern, stdout))
+
+        check_stdout(self.run_function(gen_jenkinsfile))
         self.assertTrue(os.path.exists('Jenkinsfile'))
 
         error_pattern = r"File .*/%s/Jenkinsfile already exists" % os.path.basename(self.tmpdir)
         error_pattern += ", use --force to overwrite"
-        self.assertErrorRegex(OSError, error_pattern, gen_jenkinsfile)
+        self.assertErrorRegex(OSError, error_pattern, self.run_function, gen_jenkinsfile)
 
-        gen_jenkinsfile(force=True)
+        # overwrite existing tox.ini, so we can check contents after re-generating it
+        fake_txt = "This is not a valid Jenkinsfile file"
+        write_file('Jenkinsfile', fake_txt, force=True)
+
+        check_stdout(self.run_function(gen_jenkinsfile, force=True))
+        with open('Jenkinsfile') as fp:
+            txt = fp.read()
+        self.assertTrue(txt != fake_txt)
 
     def test_tox_ini(self):
-        """Test generating of Jenkinsfile."""
+        """Test generating of tox.ini."""
         os.chdir(self.tmpdir)
-        gen_tox_ini()
 
+        def check_stdout(stdout):
+            """Helper function to check stdout output."""
+            self.assertTrue(stdout.startswith('[tox.ini]'))
+            regex = re.compile(r"^Wrote .*/%s/tox\.ini$" % self.tmpdir_name, re.M)
+            self.assertTrue(regex.search(stdout), "Pattern '%s' found in: %s" % (regex.pattern, stdout))
+
+        check_stdout(self.run_function(gen_tox_ini))
         self.assertTrue(os.path.exists('tox.ini'))
 
         # overwriting requires force
-        error_pattern = r"File .*/%s/tox.ini already exists" % os.path.basename(self.tmpdir)
+        error_pattern = r"File .*/%s/tox.ini already exists" % self.tmpdir_name
         error_pattern += ", use --force to overwrite"
-        self.assertErrorRegex(OSError, error_pattern, gen_tox_ini)
+        self.assertErrorRegex(OSError, error_pattern, self.run_function, gen_tox_ini)
 
-        gen_tox_ini(force=True)
+        # overwrite existing tox.ini, so we can check contents after re-generating it
+        fake_txt = "This is not a valid tox.ini file"
+        write_file('tox.ini', fake_txt, force=True)
+
+        check_stdout(self.run_function(gen_tox_ini, force=True))
+        with open('tox.ini') as fp:
+            txt = fp.read()
+        self.assertTrue(txt != fake_txt)
