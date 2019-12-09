45ae9a89b34f40f9cc69cbceef73081d89b04e47
Alex Domingo <alex.domingo.toro@vub.be>
2019-12-09 15:56:19 +0100

    Add Alex Domingo to the list of authors

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -140,6 +140,7 @@ wdp = ('Wouter Depypere', 'wouter.depypere@ugent.be')
 wp = ('Ward Poelmans', 'ward.poelmans@vub.be')
 sm = ('Samuel Moors', 'samuel.moors@vub.be')
 bh = ('Balazs Hajgato', 'Balazs.Hajgato@UGent.be')
+ad = ('Alex Domingo', 'alex.domingo.toro@vub.be')
 
 # Regexp used to remove suffixes from scripts when installing(/packaging)
 REGEXP_REMOVE_SUFFIX = re.compile(r'(\.(?:py|sh|pl))$')
