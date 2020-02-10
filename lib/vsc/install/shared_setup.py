412307a94be1282bce6b6234a3b5290c8d94984a
Ward Poelmans <wpoely86@gmail.com>
2020-02-10 16:46:46 +0100

    Only inject dependency version when a equal is allowed
    Inject dependency links with expliciet version is only useful when ==,
    >= or <= is used. If there is not a =, the dependency links is
    pointless.

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -162,7 +162,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.14.11'
+VERSION = '0.14.12'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 log.info('(using setuptools version %s located at %s)' % (setuptools.__version__, setuptools.__file__))
@@ -1436,7 +1436,7 @@ class vsc_setup(object):
             if dependency.startswith('vsc'):
                 dep = dependency.split(' ')[0]
                 depversion = ''
-                for comp in ['==', '<', '<=']:
+                for comp in ['==', '<=', '>=']:
                     try:
                         depversion = "-" + dependency.split(comp)[1].strip()
                     except IndexError:
