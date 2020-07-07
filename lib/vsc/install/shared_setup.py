de6d044bb833671c0e36d19e19f533b8189cc438
Wouter Depypere <wouter.depypere@ugent.be>
2020-07-07 09:49:26 +0200

    isort 5 is no longer python2 compatible

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -160,7 +160,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.15.14'
+VERSION = '0.15.15'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 log.info('(using setuptools version %s located at %s)' % (setuptools.__version__, setuptools.__file__))
@@ -1461,6 +1461,8 @@ class vsc_setup(object):
             tests_requires.append('prospector >= 1.1.6.4, < 1.3')
             # mock 4.x is no longer compatible with Python 2
             tests_requires.append('mock < 4.0')
+            # isort 5.0 is no longer compatible with Python 2
+            tests_requires.append('isort < 5.0')
         else:
             tests_requires.extend([
                 'prospector',
