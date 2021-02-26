33ca66996ac46a685bf394fbb9b01153addc5909
stdweird <stijn.deweirdt@ugent.be>
2021-02-26 15:27:29 +0100

    bump version

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -166,7 +166,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.17.2'
+VERSION = '0.17.3'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 log.info('(using setuptools version %s located at %s)' % (setuptools.__version__, setuptools.__file__))
