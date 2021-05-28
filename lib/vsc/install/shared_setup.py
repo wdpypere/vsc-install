3fb43ab2cbbdc3fd3a3c9c233938aba96b9ee412
Alex Domingo <alex.domingo.toro@vub.be>
2021-05-28 16:10:35 +0200

    version bump to 0.17.14

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -167,7 +167,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.17.13'
+VERSION = '0.17.14'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 log.info('(using setuptools version %s located at %s)' % (setuptools.__version__, setuptools.__file__))
