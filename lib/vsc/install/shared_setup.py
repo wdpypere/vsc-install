85d3b51da3ae11189910257ca6f52bf3652b7489
Ward Poelmans <wpoely86@gmail.com>
2023-08-10 16:05:07 +0200

    add python-ldap to PYTHON_BDIST_RPM_PREFIX_MAP

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -167,7 +167,7 @@ URL_GHUGENT_HPCUGENT = 'https://github.ugent.be/hpcugent/%(name)s'
 
 RELOAD_VSC_MODS = False
 
-VERSION = '0.18.7'
+VERSION = '0.18.8'
 
 log.info('This is (based on) vsc.install.shared_setup %s' % VERSION)
 log.info('(using setuptools version %s located at %s)' % (setuptools.__version__, setuptools.__file__))
@@ -181,6 +181,7 @@ NO_PREFIX_PYTHON_BDIST_RPM = ['pbs_python']
 PYTHON_BDIST_RPM_PREFIX_MAP = {
     'pycrypto': 'python%s-crypto',
     'psycopg2': 'python%s-psycopg2',
+    'python-ldap': 'python%s-ldap',
 }
 
 SHEBANG_BIN_BASH = "#!/bin/bash"
