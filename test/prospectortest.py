a8581b16c62749c2bd3e33100dea392aab13bb4a
Wouter Depypere <wouter.depypere@ugent.be>
2023-10-25 14:00:15 +0200

    black it

--- test/prospectortest.py
+++ test/prospectortest.py
@@ -37,7 +37,7 @@ from vsc.install.testing import TestCase
 
 
 class ProspectorTest(TestCase):
-    """Test ProspectorTest """
+    """Test ProspectorTest"""
 
     def setUp(self):
         """create a self.setup.instance for every test"""
@@ -47,9 +47,9 @@ class ProspectorTest(TestCase):
     def test_prospectorfail(self):
         """Test that whitelisted warnings actually fails"""
 
-        base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'prospectortest')
-        test_files = glob.glob(os.path.join(base_dir, 'lib', 'vsc', 'mockinstall', "*.py"))
-        test_files = [x for x in test_files if '__init__.py' not in x]
+        base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "prospectortest")
+        test_files = glob.glob(os.path.join(base_dir, "lib", "vsc", "mockinstall", "*.py"))
+        test_files = [x for x in test_files if "__init__.py" not in x]
         log.debug("test_files = %s", test_files)
         log.debug("base_dir = %s", base_dir)
 
@@ -63,7 +63,10 @@ class ProspectorTest(TestCase):
             all_tests.append(testfile_base)
             for failure in failures:
                 # old prospector returns strings, new prospector returns Path
-                if Path(failure['location']['path']) == Path(testfile) or testfile_base in [failure['code'], failure['message']]:
+                if Path(failure["location"]["path"]) == Path(testfile) or testfile_base in [
+                    failure["code"],
+                    failure["message"],
+                ]:
                     detected_tests.append(testfile_base)
 
         log.debug("All tests = %s", all_tests)
@@ -72,16 +75,25 @@ class ProspectorTest(TestCase):
 
         if sys.version_info[0] < 3:
             # some of the prospector test cases don't exist in Python 2
-            py2_invalid_tests = ['raising-bad-type']
+            py2_invalid_tests = ["raising-bad-type"]
             undetected_tests = [x for x in undetected_tests if x not in py2_invalid_tests]
-
 
         if sys.version_info[0] >= 3:
             # some of the prospector test cases don't make sense in Python 3 because they yield syntax errors,
             # or are no longer a problem in Python 3
-            py3_invalid_tests = ['backtick', 'old-octal-literal', 'import-star-module-level', 'redefine-in-handler',
-                                 'indexing-exception', 'old-raise-syntax', 'print-statement', 'unpacking-in-except',
-                                 'old-ne-operator', 'raising-string', 'metaclass-assignment']
+            py3_invalid_tests = [
+                "backtick",
+                "old-octal-literal",
+                "import-star-module-level",
+                "redefine-in-handler",
+                "indexing-exception",
+                "old-raise-syntax",
+                "print-statement",
+                "unpacking-in-except",
+                "old-ne-operator",
+                "raising-string",
+                "metaclass-assignment",
+            ]
             undetected_tests = [x for x in undetected_tests if x not in py3_invalid_tests]
 
         self.assertFalse(undetected_tests, f"\nprospector did not detect {undetected_tests}\n")
