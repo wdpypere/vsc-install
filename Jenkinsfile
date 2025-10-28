b97d0353c1405ea184f93bae55d42e953e1974f3
Andy Georges <itkovian@gmail.com>
2025-10-28 17:59:11 +0100

    fix: forgot closing }

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,23 +9,24 @@ node {
         sh 'git clean -fxd'
     }
     stage('test pipeline') {
-    parallel {
-        stage ('ruff format') {
-            sh 'curl -L --silent https://github.com/astral-sh/ruff/releases/download/0.13.1/ruff-x86_64-unknown-linux-gnu.tar.gz --output - | tar -xzv'
-            sh 'cp ruff-x86_64-unknown-linux-gnu/ruff .'
-            sh './ruff --version'
-            sh './ruff format --check .'
-        }
-        stage ('ruff check') {
-            sh 'curl -L --silent https://github.com/astral-sh/ruff/releases/download/0.13.1/ruff-x86_64-unknown-linux-gnu.tar.gz --output - | tar -xzv'
-            sh 'cp ruff-x86_64-unknown-linux-gnu/ruff .'
-            sh './ruff --version'
-            sh './ruff check .'
-        }
-        stage('test') {
-            sh 'pip3 install --ignore-installed --prefix $PWD/.vsc-tox tox'
-            sh 'export PATH=$PWD/.vsc-tox/bin:$PATH && export PYTHONPATH=$PWD/.vsc-tox/lib/python$(python3 -c "import sys; print(\\"%s.%s\\" % sys.version_info[:2])")/site-packages:$PYTHONPATH && tox -v -c tox.ini'
-            sh 'rm -r $PWD/.vsc-tox'
+        parallel {
+            stage ('ruff format') {
+                sh 'curl -L --silent https://github.com/astral-sh/ruff/releases/download/0.13.1/ruff-x86_64-unknown-linux-gnu.tar.gz --output - | tar -xzv'
+                sh 'cp ruff-x86_64-unknown-linux-gnu/ruff .'
+                sh './ruff --version'
+                sh './ruff format --check .'
+            }
+            stage ('ruff check') {
+                sh 'curl -L --silent https://github.com/astral-sh/ruff/releases/download/0.13.1/ruff-x86_64-unknown-linux-gnu.tar.gz --output - | tar -xzv'
+                sh 'cp ruff-x86_64-unknown-linux-gnu/ruff .'
+                sh './ruff --version'
+                sh './ruff check .'
+            }
+            stage('test') {
+                sh 'pip3 install --ignore-installed --prefix $PWD/.vsc-tox tox'
+                sh 'export PATH=$PWD/.vsc-tox/bin:$PATH && export PYTHONPATH=$PWD/.vsc-tox/lib/python$(python3 -c "import sys; print(\\"%s.%s\\" % sys.version_info[:2])")/site-packages:$PYTHONPATH && tox -v -c tox.ini'
+                sh 'rm -r $PWD/.vsc-tox'
+            }
         }
     }
 }
--- lib/vsc/install/ci.py
+++ lib/vsc/install/ci.py
@@ -499,7 +499,7 @@ def gen_jenkinsfile():
         indent("sh 'git clean -fxd'", level=2),
         indent("}"),
         indent("stage('test pipeline') {"),
-        indent("parallel {"),
+        indent("parallel {", level=2),
     ]
 
     if vsc_ci_cfg[RUN_SHELLCHECK]:
@@ -507,55 +507,56 @@ def gen_jenkinsfile():
         shellcheck_url = "https://github.com/koalaman/shellcheck/releases/download/latest/"
         shellcheck_url += "shellcheck-latest.linux.x86_64.tar.xz"
         lines.extend([
-            indent("stage ('shellcheck') {", level=2),
-            indent(f"sh 'curl -L --silent {shellcheck_url} --output - | tar -xJv'", level=3),
-            indent("sh 'cp shellcheck-latest/shellcheck .'", level=3),
-            indent("sh 'rm -r shellcheck-latest'", level=3),
-            indent("sh './shellcheck --version'", level=3),
-            indent("sh './shellcheck bin/*.sh'", level=3),
-            indent("}", level=2),
+            indent("stage ('shellcheck') {", level=3),
+            indent(f"sh 'curl -L --silent {shellcheck_url} --output - | tar -xJv'", level=4),
+            indent("sh 'cp shellcheck-latest/shellcheck .'", level=4),
+            indent("sh 'rm -r shellcheck-latest'", level=4),
+            indent("sh './shellcheck --version'", level=4),
+            indent("sh './shellcheck bin/*.sh'", level=4),
+            indent("}", level=3),
         ])
 
     r_url = f"https://github.com/astral-sh/ruff/releases/download/{RUFF_VERSION}/ruff-x86_64-unknown-linux-gnu.tar.gz"
     ruff_install_lines = [
-        indent(f"sh 'curl -L --silent {r_url} --output - | tar -xzv'", level=3),
-        indent("sh 'cp ruff-x86_64-unknown-linux-gnu/ruff .'", level=3),
-        indent("sh './ruff --version'", level=3),
+        indent(f"sh 'curl -L --silent {r_url} --output - | tar -xzv'", level=4),
+        indent("sh 'cp ruff-x86_64-unknown-linux-gnu/ruff .'", level=4),
+        indent("sh './ruff --version'", level=4),
     ]
     if vsc_ci_cfg[RUN_RUFF_FORMAT_CHECK]:
-        lines.extend([indent("stage ('ruff format') {", level=2)])
+        lines.extend([indent("stage ('ruff format') {", level=3)])
         lines.extend(ruff_install_lines)
-        lines.extend([indent("sh './ruff format --check .'", level=3), indent("}", level=2)])
+        lines.extend([indent("sh './ruff format --check .'", level=4), indent("}", level=3)])
 
     if vsc_ci_cfg[RUN_RUFF_CHECK]:
-        lines.extend([indent("stage ('ruff check') {", level=2)])
+        lines.extend([indent("stage ('ruff check') {", level=3)])
         lines.extend(ruff_install_lines)
-        lines.extend([indent("sh './ruff check .'", level=3), indent("}", level=2)])
+        lines.extend([indent("sh './ruff check .'", level=4), indent("}", level=3)])
 
-    lines.append(indent("stage('test') {", level=2))
+    lines.append(indent("stage('test') {", level=3))
     for test_cmd in test_cmds:
         # be careful with test commands that include single quotes!
         if "'" in test_cmd:
-            lines.append(indent(f'sh """{test_cmd}"""', level=3))
+            lines.append(indent(f'sh """{test_cmd}"""', level=4))
         else:
-            lines.append(indent(f"sh '{test_cmd}'", level=3))
-    lines.append(indent("}", level=2))
+            lines.append(indent(f"sh '{test_cmd}'", level=4))
+    lines.append(indent("}", level=3))
 
     if vsc_ci_cfg[JIRA_ISSUE_ID_IN_PR_TITLE]:
         lines.extend([
-            indent("stage('PR title JIRA link') {", level=2),
-            indent("if (env.CHANGE_ID) {", level=3),
-            indent(r"if (env.CHANGE_TITLE =~ /\s+\(?HPC-\d+\)?/) {", level=4),
-            indent('echo "title ${env.CHANGE_TITLE} seems to contain JIRA ticket number."', level=5),
-            indent("} else {", level=4),
-            indent("echo \"ERROR: title ${env.CHANGE_TITLE} does not end in 'HPC-number'.\"", level=5),
-            indent('error("malformed PR title ${env.CHANGE_TITLE}.")', level=5),
+            indent("stage('PR title JIRA link') {", level=3),
+            indent("if (env.CHANGE_ID) {", level=4),
+            indent(r"if (env.CHANGE_TITLE =~ /\s+\(?HPC-\d+\)?/) {", level=5),
+            indent('echo "title ${env.CHANGE_TITLE} seems to contain JIRA ticket number."', level=6),
+            indent("} else {", level=5),
+            indent("echo \"ERROR: title ${env.CHANGE_TITLE} does not end in 'HPC-number'.\"", level=6),
+            indent('error("malformed PR title ${env.CHANGE_TITLE}.")', level=6),
+            indent("}", level=5),
             indent("}", level=4),
             indent("}", level=3),
-            indent("}", level=2),
         ])
 
     # close parallel stages
+    lines.append(indent("}", level=2))
     lines.append(indent("}"))
 
     lines.append("}")
